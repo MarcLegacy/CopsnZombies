04e4b090e21294a0b0c1aa85eab2f35b927de03c
Marc Meijering <marc_1997@live.nl>
2023-01-17 14:12:33 +0100

    Initial commit

---
+++ Source/CopsnZombies/CopsnZombies.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CopsnZombies : ModuleRules
+{
+	public CopsnZombies(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/CopsnZombies/CopsnZombiesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CopsnZombiesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACopsnZombiesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACopsnZombiesGameMode();
+};
+
+
+
